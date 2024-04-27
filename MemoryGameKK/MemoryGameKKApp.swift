bceb7fc7775fceb7de81990e1c4540573608207b
Konrad Kalman <k.konrad9902@gmail.com>
2024-04-27 11:20:06 +0200

    Initial Commit

---
+++ MemoryGameKK/MemoryGameKKApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameKKApp.swift
+//  MemoryGameKK
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameKKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
